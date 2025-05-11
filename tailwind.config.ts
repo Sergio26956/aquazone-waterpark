806ceee8f723783e472bf0a409fa9b95cbb5f97d
sergio <sergiolopezantequera@gmail.com>
2025-05-11 03:39:18 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,11 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#00BFFF',
-        secondary: '#1E90FF'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
