2b34c926be9a61977a3e24b8af85e56f373d5255
sergio <sergiolopezantequera@gmail.com>
2025-05-11 02:26:37 +0200

    Create page.tsx

---
+++ app/flotantes/piscinas/page.tsx
@@ -0,0 +1,8 @@
+export default function PiscinasPage() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold text-blue-600">Flotantes en Piscinas</h2>
+      <p>Parques modulares diseñados para piscinas con máxima diversión acuática.</p>
+    </div>
+  )
+}
