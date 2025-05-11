d98a9d67773a4f2694599861a2cc2aed22a40b91
sergio <sergiolopezantequera@gmail.com>
2025-05-11 02:23:19 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from '@/components/HeroSection'
+import FloatingCallButton from '@/components/FloatingCallButton'
+import SocialBar from '@/components/SocialBar'
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      <SocialBar />
+      <FloatingCallButton />
+    </main>
+  )
+}
