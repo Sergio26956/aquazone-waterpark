008eadb40dc131ab1fea6c695cf0137dcc64e3cf
sergio <sergiolopezantequera@gmail.com>
2025-05-11 02:33:52 +0200

    Create page.tsx

---
+++ app/contacto/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from '@/components/ContactForm'
+
+export default function ContactoPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-blue-800">Contacto</h1>
+      <ContactForm />
+    </div>
+  )
+}
