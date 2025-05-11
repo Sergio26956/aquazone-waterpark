91d67ffb91d07777afdab72b180aa53407436360
sergio <sergiolopezantequera@gmail.com>
2025-05-11 02:41:07 +0200

    Create FloatingCallButton.tsx

---
+++ components/FloatingCallButton.tsx
@@ -0,0 +1,10 @@
+export default function FloatingCallButton() {
+  return (
+    <a
+      href="tel:+34600000000"
+      className="fixed bottom-6 right-6 bg-green-600 text-white px-4 py-2 rounded-full shadow-lg hover:bg-green-700 transition"
+    >
+      Llamar
+    </a>
+  )
+}
