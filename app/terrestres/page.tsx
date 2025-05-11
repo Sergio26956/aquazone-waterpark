56a77f217d6d2f467e34a7839bbf67d7e4a7370f
sergio <sergiolopezantequera@gmail.com>
2025-05-11 02:29:53 +0200

    Create page.tsx

---
+++ app/terrestres/page.tsx
@@ -0,0 +1,8 @@
+export default function TerrestresPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-green-700">Parques Acuáticos Terrestres</h1>
+      <p>Conoce nuestras atracciones acuáticas móviles y fijas para entornos urbanos.</p>
+    </div>
+  )
+}
