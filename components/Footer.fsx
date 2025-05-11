6742bc9d6160e773839192265de025d633200866
sergio <sergiolopezantequera@gmail.com>
2025-05-11 02:37:22 +0200

    Create Footer.fsx

---
+++ components/Footer.fsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="bg-blue-900 text-white text-center p-4 mt-10">
+      <p>&copy; {new Date().getFullYear()} AQUAZONE Water Park. Todos los derechos reservados.</p>
+    </footer>
+  )
+}
