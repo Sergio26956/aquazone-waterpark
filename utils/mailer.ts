196e73621efc6eec22eef257e349e80f037e61e1
sergio <sergiolopezantequera@gmail.com>
2025-05-11 04:20:51 +0200

    Update mailer.ts

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 
 export async function sendMail({ name, email, message }: { name: string; email: string; message: string }) {
   const transporter = nodemailer.createTransport({
@@ -7,19 +7,24 @@ export async function sendMail({ name, email, message }: { name: string; email: 
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
     },
-  })
+  });
 
   const mailOptions = {
     from: email,
     to: process.env.MAIL_RECEIVER,
-    subject: `Nuevo mensaje de ${name}`,
+    subject: `Nuevo mensaje de contacto de ${name}`,
     text: message,
-  }
+    html: `
+      <p><strong>Nombre:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Mensaje:</strong><br>${message}</p>
+    `,
+  };
 
   try {
-    const info = await transporter.sendMail(mailOptions)
-    return { success: true, info }
+    await transporter.sendMail(mailOptions);
+    return { success: true };
   } catch (error) {
-    return { success: false, error }
+    return { success: false, error };
   }
 }
