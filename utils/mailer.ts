3da03d6c0bdaca71fc550af9035ff6475f4d4070
sergio <sergiolopezantequera@gmail.com>
2025-05-11 03:36:33 +0200

    Create mailer.ts

---
+++ utils/mailer.ts
@@ -0,0 +1,9 @@
+export async function sendContactForm(data: {
+  nombre: string
+  email: string
+  mensaje: string
+}) {
+  // Simulación de envío (reemplazar por integración real si es necesario)
+  console.log('Enviando mensaje de contacto:', data)
+  return true
+}
