592ddbb7483cc5805e77856a76144a2a8304dcb5
sergio <sergiolopezantequera@gmail.com>
2025-05-14 07:22:37 +0200

    Create SectionDivider.tsx

---
+++ components/SectionDivider.tsx
@@ -0,0 +1,7 @@
+export default function SectionDivider() {
+  return (
+    <div className="w-full my-12">
+      <div className="h-1 w-1/2 bg-gradient-to-r from-blue-500 via-sky-400 to-blue-500 mx-auto rounded-full animate-pulse" />
+    </div>
+  )
+}
