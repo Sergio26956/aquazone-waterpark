f6b67b254621399d45cffb40e79e7fa39ca29cbe
sergio <sergiolopezantequera@gmail.com>
2025-05-14 08:15:46 +0200

    Create About.tsx

---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-8 rounded-xl shadow-xl">
+      <h2 className="text-2xl font-bold mb-4">Sobre Nosotros</h2>
+      <p>
+        AQUAZONE Water Park es una empresa de referencia en parques acuáticos modulares y urbanos, especializada en eventos
+        acuáticos para todas las edades. Con sedes móviles en diferentes puntos de España, llevamos diversión y seguridad a
+        playas, piscinas y ciudades.
+      </p>
+    </section>
+  )
+}
