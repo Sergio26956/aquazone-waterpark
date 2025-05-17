8a194cef4bc089ee2fd6ba09b358e29cce2a5da2
sergio <sergiolopezantequera@gmail.com>
2025-05-17 20:28:39 +0200

    Create admin.tsx

---
+++ app/(admin)/admin.tsx
@@ -0,0 +1,9 @@
+import AdminPanel from "@/components/AdminPanel";
+
+export default function AdminPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 p-6">
+      <AdminPanel />
+    </main>
+  );
+}
