23b9ec317da94f0e623db26c18e2a68d83d9ba74
sergio <sergiolopezantequera@gmail.com>
2025-05-17 20:25:56 +0200

    Create data.ts

---
+++ lib/data.ts
@@ -0,0 +1,18 @@
+export const parques = {
+  flotantes: {
+    piscinas: [
+      { nombre: "Piscina Modular XL", descripcion: "Gran capacidad y seguridad", imagen: "/images/piscina1.jpg" },
+    ],
+    playas: [
+      { nombre: "Playa Acuática Flotante", descripcion: "Diseño ideal para costas", imagen: "/images/playa1.jpg" },
+    ],
+  },
+  urbanos: {
+    actividades: [
+      { nombre: "Tobogán Extremo", descripcion: "Diversión garantizada", imagen: "/images/urbano1.jpg" },
+    ],
+    kamikazeJump: [
+      { nombre: "Kamikaze Jump", descripcion: "Salto de adrenalina", imagen: "/images/kamikaze1.jpg" },
+    ],
+  },
+};
