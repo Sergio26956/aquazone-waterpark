6026dd29d4c94f59398b20793070c54d9bd5b9b0
sergio <sergiolopezantequera@gmail.com>
2025-05-18 10:20:52 +0200

    Create page.tsx

---
+++ app/(web)/legal/terminos/page.tsx
@@ -0,0 +1,9 @@
+import { LegalTerms } from "@/components/sections/legal-terms"
+
+export default function TerminosPage() {
+  return (
+    <main className="pt-20">
+      <LegalTerms />
+    </main>
+  )
+}
