98019e328cdcc4b31f7d442972529002506f8dbb
sergio <sergiolopezantequera@gmail.com>
2025-05-18 08:29:44 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,7 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "AQUAZONE Water Park",
-  description: "Parques acuáticos modulares y móviles por Alqueva Producciones",
-};
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  return (
-    <html lang="es">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+  return children
 }
