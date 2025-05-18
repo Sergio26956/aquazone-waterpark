2748030cf0e40be1c4f0a48c4d105b2ba0f98e8a
sergio <sergiolopezantequera@gmail.com>
2025-05-18 10:05:28 +0200

    Create page.tsx

---
+++ app/(web)/urbanos/page.tsx
@@ -0,0 +1,9 @@
+import { UrbanParks } from "@/components/sections/urban-parks"
+
+export default function UrbanParksPage() {
+  return (
+    <main className="pt-20">
+      <UrbanParks />
+    </main>
+  )
+}
