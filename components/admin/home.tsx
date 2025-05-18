05d4963fd4dab20b9e10bad4ac992f33c678d40a
sergio <sergiolopezantequera@gmail.com>
2025-05-18 10:33:11 +0200

    Update home.tsx

--- components/admin/home.tsx
+++ components/admin/home.tsx
@@ -1,17 +1,15 @@
+import { AdminCalendar } from "./calendar"
+import { AdminUsers } from "./users"
+
 export function AdminHome() {
   return (
     <section className="p-10">
       <h1 className="text-3xl font-bold mb-4">Panel de Administración</h1>
       <p className="mb-6 text-neutral-600">Bienvenido. Desde aquí puedes gestionar tus actividades.</p>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
-        <div className="border p-4 rounded shadow hover:shadow-lg transition">
-          <h2 className="font-semibold mb-2">Calendario de reservas</h2>
-          <p>Visualiza y organiza las fechas contratadas.</p>
-        </div>
-        <div className="border p-4 rounded shadow hover:shadow-lg transition">
-          <h2 className="font-semibold mb-2">Gestión de usuarios</h2>
-          <p>Accede al registro de usuarios registrados.</p>
-        </div>
+
+      <div className="grid gap-8">
+        <AdminCalendar />
+        <AdminUsers />
       </div>
     </section>
   )
