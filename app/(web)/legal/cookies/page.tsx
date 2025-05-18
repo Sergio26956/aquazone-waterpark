1103bda1a3a8ebfb81500d0d967871a59392efeb
sergio <sergiolopezantequera@gmail.com>
2025-05-18 10:15:02 +0200

    Create page.tsx

---
+++ app/(web)/legal/cookies/page.tsx
@@ -0,0 +1,9 @@
+import { LegalCookies } from "@/components/sections/legal-cookies"
+
+export default function CookiesPage() {
+  return (
+    <main className="pt-20">
+      <LegalCookies />
+    </main>
+  )
+}
