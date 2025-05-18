5089434ca3a41318bad4aad9dfbfacdb59baf8bf
sergio <sergiolopezantequera@gmail.com>
2025-05-18 10:04:24 +0200

    Create page.tsx

---
+++ app/(web)/flotantes/page.tsx
@@ -0,0 +1,9 @@
+import { FloatingParks } from "@/components/sections/floating-parks"
+
+export default function FloatingParksPage() {
+  return (
+    <main className="pt-20">
+      <FloatingParks />
+    </main>
+  )
+}
