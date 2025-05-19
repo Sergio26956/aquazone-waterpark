5dd694682760bd11237e1649fad4a44adca7519f
sergio <sergiolopezantequera@gmail.com>
2025-05-19 03:18:16 +0200

    Create page.tsx

---
+++ app/(admin)/usuarios/page.tsx
@@ -0,0 +1,9 @@
+import ListaUsuarios from '@/components/admin/ListaUsuarios';
+
+export default function UsuariosPage() {
+  return (
+    <main className="p-6 bg-gray-50 min-h-screen">
+      <ListaUsuarios />
+    </main>
+  );
+}
