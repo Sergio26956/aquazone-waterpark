9940611fbe4487974cb11633920b25548bb42151
sergio <sergiolopezantequera@gmail.com>
2025-05-19 02:57:50 +0200

    Create AdminLayout.tsx

---
+++ components/admin/AdminLayout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import AdminSidebar from './AdminSidebar';
+import AdminHeader from './AdminHeader';
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <AdminSidebar />
+      <div className="flex-1">
+        <AdminHeader />
+        <main className="p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
