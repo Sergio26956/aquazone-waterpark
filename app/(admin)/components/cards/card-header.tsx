1c1414c710062911a7ee1ccdd90a876d3009c943
sergio <sergiolopezantequera@gmail.com>
2025-05-19 06:08:29 +0200

    Create card-header.tsx

---
+++ app/(admin)/components/cards/card-header.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+interface CardHeaderProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function CardHeader({ className, children }: CardHeaderProps) {
+  return (
+    <div className={cn("flex flex-col space-y-1.5 p-6", className)}>
+      {children}
+    </div>
+  );
+}
