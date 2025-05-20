982ad035f7be31c5a02978fe27c4307eedb92488
sergio <sergiolopezantequera@gmail.com>
2025-05-20 04:51:25 +0200

    Create use-toast.tsx

---
+++ app/(admin)/components/ui/use-toast.tsx
@@ -0,0 +1,21 @@
+import {
+  ToastProvider,
+  ToastViewport,
+} from "@/app/(admin)/components/ui/toast-toast-provider";
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastTitle,
+} from "@/app/(admin)/components/ui/toast-toast";
+import { useToast } from "@/components/ui/use-toast-core";
+
+export {
+  useToast,
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastTitle,
+};
