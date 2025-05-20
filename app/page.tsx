a359e3593247b4a2a2422984bdf80d9f0d5d8d9e
sergio <sergiolopezantequera@gmail.com>
2025-05-20 06:19:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import CategoryGrid from "./components/CategoryGrid";
+import Footer from "./components/Footer";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-black">
+      <Navbar />
       <Hero />
       <CategoryGrid />
+      <Footer />
     </main>
   );
 }
