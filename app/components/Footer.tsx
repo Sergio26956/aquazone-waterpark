4720f377fdd96330d4d035fed20ae10933f766b2
sergio <sergiolopezantequera@gmail.com>
2025-05-20 06:17:56 +0200

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-950 text-white text-center py-6 mt-20">
+      <p className="text-sm">&copy; 2025 AQUAZONE Water Park. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
