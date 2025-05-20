f0e4b6dc4e4615dfc26ea045971da207ca9c51fb
sergio <sergiolopezantequera@gmail.com>
2025-05-20 04:37:22 +0200

    Create toast-toast-viewport.tsx

---
+++ app/(admin)/components/ui/toast-toast-viewport.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import * as React from "react";
+import * as ToastPrimitives from "@radix-ui/react-toast";
+import { cn } from "@/lib/utils";
+
+const ToastViewport = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitives.Viewport>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitives.Viewport>
+>(({ className, ...props }, ref) => (
+  <ToastPrimitives.Viewport
+    ref={ref}
+    className={cn(
+      "fixed top-0 z-50 flex max-h-screen w-full flex-col-reverse p-4 sm:top-auto sm:right-0 sm:flex-col md:max-w-[420px]",
+      className
+    )}
+    {...props}
+  />
+));
+ToastViewport.displayName = ToastPrimitives.Viewport.displayName;
+
+export { ToastViewport };
