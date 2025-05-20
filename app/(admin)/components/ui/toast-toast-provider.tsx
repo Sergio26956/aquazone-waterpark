4d80ca8bb75f32dd64e1290e37eea10b36db4b5e
sergio <sergiolopezantequera@gmail.com>
2025-05-20 04:33:22 +0200

    Create toast-toast-provider.tsx

---
+++ app/(admin)/components/ui/toast-toast-provider.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import * as React from "react";
+import * as ToastPrimitives from "@radix-ui/react-toast";
+
+const ToastProvider = ToastPrimitives.Provider;
+
+export { ToastProvider };
