a52b86662e4c3e1a155c34c7a80e25492185e542
sergio <sergiolopezantequera@gmail.com>
2025-05-20 06:25:38 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,11 @@
-import type { Config } from "tailwindcss"
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#2563EB",
-        secondary: "#38BDF8",
-      },
-      animation: {
-        pulseGlow: "pulse-glow 2s infinite",
-        fadeInUp: "fadeInUp 0.8s ease forwards",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
-
-export default config
+};
