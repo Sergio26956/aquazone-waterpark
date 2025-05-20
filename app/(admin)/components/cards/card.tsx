7c9e36730882ef9be42f800643bb85461ea59fb6
sergio <sergiolopezantequera@gmail.com>
2025-05-20 04:05:14 +0200

    Update card.tsx

--- app/(admin)/components/cards/card.tsx
+++ app/(admin)/components/cards/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export function Card({ className, children }: CardProps) {
   return (
-    <div className={cn("rounded-2xl border bg-card text-card-foreground shadow", className)}>
+    <div className={cn("rounded-xl border bg-card text-card-foreground shadow", className)}>
       {children}
     </div>
   );
