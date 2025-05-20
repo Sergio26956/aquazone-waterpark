d3b1ae25bbe78ee81fa23973484227ee4b5cf035
sergio <sergiolopezantequera@gmail.com>
2025-05-20 05:10:27 +0200

    Create form.tsx

---
+++ components/ui/form.tsx
@@ -0,0 +1,19 @@
+import {
+  Form as FormBase,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "@radix-ui/react-form"
+
+export const Form = FormBase
+export {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+}
