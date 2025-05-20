2992ae01bfece0f8c7de928349899ebfb0a1200b
sergio <sergiolopezantequera@gmail.com>
2025-05-20 06:29:36 +0200

    Create page.tsx

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminDashboard() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Panel de Control</h1>
+      <p>Bienvenido al panel de administración de AQUAZONE.</p>
+    </div>
+  );
+}
