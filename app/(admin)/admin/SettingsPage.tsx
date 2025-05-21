9106b8a01b7ebdef3c36139e8d58811da2198c3e
sergio <sergiolopezantequera@gmail.com>
2025-05-21 06:59:10 +0200

    Create SettingsPage.tsx

---
+++ app/(admin)/admin/SettingsPage.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Configuración General</h2>
+      <p>Ajustes y parámetros de la página web.</p>
+    </div>
+  )
+}
+
+export default SettingsPage
