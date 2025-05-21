771ed2823e7c3735fd5c6da7977d9118fc955d65
sergio <sergiolopezantequera@gmail.com>
2025-05-21 06:49:01 +0200

    Create UsersPage.tsx

---
+++ app/(admin)/admin/UsersPage.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const UsersPage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Gestión de Usuarios</h2>
+      <p>Aquí podrás ver y administrar los usuarios registrados en la web.</p>
+    </div>
+  )
+}
+
+export default UsersPage
