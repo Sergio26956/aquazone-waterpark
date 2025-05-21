5cc93a1fef38779ce2d6ea86cb70b89932a7d0e2
sergio <sergiolopezantequera@gmail.com>
2025-05-21 07:06:49 +0200

    Create page.tsx

---
+++ app/(admin)/admin/(routes)/deshboard/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Panel de Control</h1>
+      <p className="mt-2">Bienvenido al área de administración de AQUAZONE Water Park.</p>
+    </div>
+  )
+}
+
+export default DashboardPage
