cca261ec5f735d73de1e419ea5136e0e14760541
sergio <sergiolopezantequera@gmail.com>
2025-06-07 07:21:30 +0200

    Create seo.ts

---
+++ src/components/admin/lib/seo.ts
@@ -0,0 +1,16 @@
+export const seo = {
+  title: 'AQUAZONE Water Park - Parques Acuáticos Flotantes y Urbanos',
+  description:
+    'Descubre la experiencia inolvidable de nuestros parques acuáticos móviles y flotantes. Diversión sin límites para todas las edades.',
+  openGraph: {
+    type: 'website',
+    locale: 'es_ES',
+    url: 'https://aquazonewaterpark.com',
+    site_name: 'AQUAZONE Water Park',
+  },
+  twitter: {
+    handle: '@aquazone',
+    site: '@aquazone',
+    cardType: 'summary_large_image',
+  },
+};
