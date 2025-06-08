1ac8ea3912c688a8d158f79ebee7416f86d9be28
sergio <sergiolopezantequera@gmail.com>
2025-06-08 03:52:05 +0200

    Create MainLayout.tsx

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer';
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
