a8a76f7068ebc96c8543f75323c24860e1585f8e
sergio <sergiolopezantequera@gmail.com>
2025-06-08 04:18:39 +0200

    Create page.tsx

---
+++ src/app/parques/urbanos/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function ParquesUrbanos() {
+  return (
+    <div className="p-8">
+      <h2 className="text-3xl font-bold mb-4">Parques Acuáticos Urbanos</h2>
+      <p>Parques terrestres móviles. Kamikaze Jump, toboganes, obstáculos, y más.</p>
+    </div>
+  );
+}
