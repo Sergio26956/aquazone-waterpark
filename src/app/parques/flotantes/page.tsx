419d4102f60d83424675ec99b54369d466a9a413
sergio <sergiolopezantequera@gmail.com>
2025-06-08 04:14:56 +0200

    Create page.tsx

---
+++ src/app/parques/flotantes/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function ParquesFlotantes() {
+  return (
+    <div className="p-8">
+      <h2 className="text-3xl font-bold mb-4">Parques Acuáticos Flotantes</h2>
+      <p>Incluye módulos para playas y piscinas. Diversión sobre el agua.</p>
+    </div>
+  );
+}
