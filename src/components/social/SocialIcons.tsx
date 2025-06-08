eb1a4076bf326494a877825658958b1137c7c13f
sergio <sergiolopezantequera@gmail.com>
2025-06-08 03:03:15 +0200

    Update SocialIcons.tsx

--- src/components/social/SocialIcons.tsx
+++ src/components/social/SocialIcons.tsx
@@ -1,11 +1,18 @@
-import { FaInstagram, FaFacebook, FaTiktok } from 'react-icons/fa';
+'use client';
+import { FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa';
 
 export default function SocialIcons() {
   return (
-    <div className="flex gap-4 text-2xl text-white">
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
-      <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer"><FaTiktok /></a>
+    <div className="flex space-x-4 text-white text-2xl">
+      <a href="https://facebook.com/aquazone" target="_blank" rel="noopener noreferrer">
+        <FaFacebookF />
+      </a>
+      <a href="https://instagram.com/aquazone" target="_blank" rel="noopener noreferrer">
+        <FaInstagram />
+      </a>
+      <a href="https://tiktok.com/@aquazone" target="_blank" rel="noopener noreferrer">
+        <FaTiktok />
+      </a>
     </div>
   );
 }
