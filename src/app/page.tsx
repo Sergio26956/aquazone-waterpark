25c13f949a197e6a99ee7832904e5571037f0e18
sergio <sergiolopezantequera@gmail.com>
2025-06-08 04:10:14 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import HeroSection from '@/components/ui/HeroSection';
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      {/* Aquí irán más secciones como categorías, videos, testimonios... */}
+    </>
+  );
+}
