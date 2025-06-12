d2da5ce45aba92c7d6bd3f030f319ec75afe0d30
sergio <sergiolopezantequera@gmail.com>
2025-06-12 03:49:27 +0200

    Create GradientBackground.tsx

---
+++ src/components/ui/GradientBackground.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+export default function GradientBackground() {
+  return (
+    <div className="fixed inset-0 -z-10 bg-gradient-to-br from-sky-100 to-blue-300"></div>
+  );
+}
