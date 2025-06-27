91709003bfcfe399e79d89aae43a55b6a748e9dc
sergio <sergiolopezantequera@gmail.com>
2025-06-27 04:47:45 +0200

    Delete app/(admin)/admin.tsx

--- app/(admin)/admin.tsx
+++
@@ -1,9 +0,0 @@
-import AdminPanel from "@/components/AdminPanel";
-
-export default function AdminPage() {
-  return (
-    <main className="min-h-screen bg-gray-50 p-6">
-      <AdminPanel />
-    </main>
-  );
-}
