981cc3de92953988c3d2bb579eb004029e5e4d17
sergio <sergiolopezantequera@gmail.com>
2025-06-27 05:04:56 +0200

    Delete app/flotantes directory

--- app/flotantes/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function FlotantesPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold text-blue-700">Parques Acuáticos Flotantes</h1>
-      <p>Explora nuestras opciones flotantes: en piscinas o playas naturales.</p>
-    </div>
-  )
-}
