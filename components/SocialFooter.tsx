67a628212e01839cb3e0d2a1dee569b392034b76
sergio <sergiolopezantequera@gmail.com>
2025-06-29 01:30:17 +0200

    Update SocialFooter.tsx

--- components/SocialFooter.tsx
+++ components/SocialFooter.tsx
@@ -1,13 +1,13 @@
-"use client";
-import { FaInstagram, FaFacebook, FaTiktok, FaYoutube } from "react-icons/fa";
+'use client'
+
+import React from 'react'
 
 export default function SocialFooter() {
   return (
-    <div className="flex justify-center gap-6 py-6 bg-neutral-900 text-white">
-      <a href="https://instagram.com" target="_blank"><FaInstagram size={22} /></a>
-      <a href="https://facebook.com" target="_blank"><FaFacebook size={22} /></a>
-      <a href="https://tiktok.com" target="_blank"><FaTiktok size={22} /></a>
-      <a href="https://youtube.com" target="_blank"><FaYoutube size={22} /></a>
-    </div>
-  );
+    <footer className="w-full bg-black bg-opacity-70 text-white text-center py-4 absolute bottom-0 z-40">
+      <p className="text-sm">
+        © {new Date().getFullYear()} AQUAZONE Water Park · Todos los derechos reservados
+      </p>
+    </footer>
+  )
 }
