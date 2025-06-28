d046b52a18473044b267e9bae3f972a95d60a256
sergio <sergiolopezantequera@gmail.com>
2025-06-28 09:15:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import MainMenu from "@/components/MainMenu";
+import Secciones from "@/components/Secciones";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen w-full overflow-x-hidden">
       <MainMenu />
       <Hero />
+      <Secciones />
     </main>
   );
 }
