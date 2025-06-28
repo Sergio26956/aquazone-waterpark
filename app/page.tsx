bd606c88f2ad512279011ba5eb8fe60019535c6c
sergio <sergiolopezantequera@gmail.com>
2025-06-28 09:23:06 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import MainMenu from "@/components/MainMenu";
 import Secciones from "@/components/Secciones";
+import Beneficios from "@/components/Beneficios";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <MainMenu />
       <Hero />
       <Secciones />
+      <Beneficios />
     </main>
   );
 }
