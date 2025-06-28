42947f8926bb61205d95bb4d82aeab036ca2e381
sergio <sergiolopezantequera@gmail.com>
2025-06-28 06:13:01 +0200

    Update SocialTabs.tsx

--- components/SocialTabs.tsx
+++ components/SocialTabs.tsx
@@ -1,12 +1,18 @@
 'use client'
+
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+
 export default function SocialTabs() {
   return (
-    <div className="fixed left-4 bottom-24 flex flex-col gap-3 z-50">
-      <a href="https://www.facebook.com" target="_blank" className="bg-blue-600 text-white px-3 py-2 rounded shadow">
-        Facebook
+    <div className="flex justify-center gap-4 p-4">
+      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <FaFacebook size={24} className="hover:text-blue-600 transition" />
       </a>
-      <a href="https://www.instagram.com" target="_blank" className="bg-pink-500 text-white px-3 py-2 rounded shadow">
-        Instagram
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <FaInstagram size={24} className="hover:text-pink-500 transition" />
+      </a>
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <FaTwitter size={24} className="hover:text-blue-400 transition" />
       </a>
     </div>
   )
