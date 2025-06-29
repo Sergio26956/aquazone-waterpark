a8e8a5791a2210b77a6468a9fa5808ca8080b15a
sergio <sergiolopezantequera@gmail.com>
2025-06-29 06:27:44 +0200

    Delete app/(web)/urbanos directory

--- app/(web)/urbanos/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UrbanParks } from "@/components/sections/urban-parks"
-
-export default function UrbanParksPage() {
-  return (
-    <main className="pt-20">
-      <UrbanParks />
-    </main>
-  )
-}
