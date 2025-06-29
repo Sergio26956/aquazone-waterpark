653296c22e1411d4802b9fb807460158423bdc90
sergio <sergiolopezantequera@gmail.com>
2025-06-29 10:55:21 +0200

    Delete app/admin directory

--- app/admin/calendario/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CalendarioPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Calendario</h1>
-      <p>Fechas contratadas y planificación de eventos.</p>
-    </div>
-  );
-}
