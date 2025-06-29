84a5a02315d6e1d7259a13b3eb4810ba02721c41
sergio <sergiolopezantequera@gmail.com>
2025-06-29 06:37:47 +0200

    Create page.tsx

---
+++ app/parques/terrestres/kamikaze/page.tsx
@@ -0,0 +1,8 @@
+export default function KamikazePage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Kamikaze Jump</h1>
+      <p>Atracción extrema para los más valientes. Adrenalina garantizada.</p>
+    </div>
+  );
+}
