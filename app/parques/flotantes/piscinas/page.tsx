c8e632a98c8e26cca47ae5b1117020532028f267
sergio <sergiolopezantequera@gmail.com>
2025-06-29 06:36:16 +0200

    Create page.tsx

---
+++ app/parques/flotantes/piscinas/page.tsx
@@ -0,0 +1,8 @@
+export default function PiscinasFlotantesPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Parques Flotantes en Piscinas</h1>
+      <p>Instalaciones flotantes diseñadas para piscinas privadas o públicas.</p>
+    </div>
+  );
+}
