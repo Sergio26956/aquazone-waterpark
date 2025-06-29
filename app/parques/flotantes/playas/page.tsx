de77ed73d307fa1eed3837c9a0902f7d355ab0fc
sergio <sergiolopezantequera@gmail.com>
2025-06-29 06:34:56 +0200

    Create page.tsx

---
+++ app/parques/flotantes/playas/page.tsx
@@ -0,0 +1,8 @@
+export default function PlayasFlotantesPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Parques Flotantes en Playas</h1>
+      <p>Explora nuestras atracciones acuáticas instaladas en playas.</p>
+    </div>
+  );
+}
