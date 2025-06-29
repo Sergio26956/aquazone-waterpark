eb05044dca2b5bd5ef5421ffeb97367b045908e2
sergio <sergiolopezantequera@gmail.com>
2025-06-29 06:27:29 +0200

    Delete app/(web)/flotantes directory

--- app/(web)/flotantes/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FloatingParks } from "@/components/sections/floating-parks"
-
-export default function FloatingParksPage() {
-  return (
-    <main className="pt-20">
-      <FloatingParks />
-    </main>
-  )
-}
