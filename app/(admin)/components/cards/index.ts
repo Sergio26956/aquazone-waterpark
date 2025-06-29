72b409aab9ccf3a9d2beec2a11e6f52625d378c2
sergio <sergiolopezantequera@gmail.com>
2025-06-29 10:54:12 +0200

    Delete app/(admin) directory

--- app/(admin)/components/cards/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./card-title";
-export * from "./card-description";
-export * from "./card-header";
-export * from "./card-content";
-export * from "./card-footer";
--- app/(admin)/components/ui/toast-toast-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import * as React from "react";
-import * as ToastPrimitives from "@radix-ui/react-toast";
-
-const ToastProvider = ToastPrimitives.Provider;
-
-export { ToastProvider };
