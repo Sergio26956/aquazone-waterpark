69d09068f583274e81080658d7f53274967eb5e1
sergio <sergiolopezantequera@gmail.com>
2025-06-30 02:15:41 +0200

    Update ScrollIndicator.tsx

--- components/ui/ScrollIndicator.tsx
+++ components/ui/ScrollIndicator.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import { motion } from 'framer-motion'
+
 export default function ScrollIndicator() {
   return (
-    <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-30">
-      <div className="w-6 h-10 border-2 border-white rounded-full flex items-start justify-center">
-        <div className="w-1 h-2 bg-white animate-bounce mt-1 rounded-full" />
-      </div>
+    <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-20">
+      <motion.div
+        animate={{ y: [0, 10, 0] }}
+        transition={{ duration: 1.5, repeat: Infinity }}
+        className="w-6 h-6 border-2 border-white rounded-full flex items-center justify-center"
+      >
+        <div className="w-2 h-2 bg-white rounded-full" />
+      </motion.div>
     </div>
   )
 }
