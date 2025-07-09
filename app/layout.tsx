1d5a018bb2d329e8a1e4808fdd6a3b5c2760f862
sergio <sergiolopezantequera@gmail.com>
2025-07-09 05:51:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,15 @@ import { ReactNode } from 'react';
 export const metadata = {
   title: 'AQUAZONE WaterPark',
   description: 'Parques acuáticos flotantes y urbanos de nueva generación.',
+  icons: {
+    icon: '/icons/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
+      <head />
       <body className="bg-black text-white">
         <NavBar />
         <div className="pt-20">{children}</div>
