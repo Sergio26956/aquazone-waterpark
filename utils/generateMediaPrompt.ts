0dc48a004cbd31cfd866a8bbf1389313789ce07c
sergio <sergiolopezantequera@gmail.com>
2025-07-12 02:21:19 +0200

    Create generateMediaPrompt.ts

---
+++ utils/generateMediaPrompt.ts
@@ -0,0 +1,3 @@
+export function generateMediaPrompt(eventType: string, style: string) {
+  return `Crea una imagen o video promocional impactante y moderno para el evento "${eventType}" en el parque acuático AQUAZONE. Estilo ${style}, colores vibrantes, enfoque en diversión y tecnología avanzada.`;
+}
