0c36b95ecfa39658fd853f3673f472acfdc45f5f
sergio <sergiolopezantequera@gmail.com>
2025-07-12 12:59:06 +0200

    Update SocialBar.tsx

--- components/SocialBar.tsx
+++ components/SocialBar.tsx
@@ -1,9 +1,10 @@
+'use client';
 export default function SocialBar() {
   return (
-    <div className="fixed left-4 top-1/2 transform -translate-y-1/2 flex flex-col space-y-3">
-      <a href="https://facebook.com" target="_blank" className="bg-blue-600 p-2 rounded-full text-white">FB</a>
-      <a href="https://instagram.com" target="_blank" className="bg-pink-500 p-2 rounded-full text-white">IG</a>
-      <a href="https://tiktok.com" target="_blank" className="bg-black p-2 rounded-full text-white">TT</a>
+    <div className="flex justify-center gap-4 p-4 bg-gray-800 text-white">
+      <a href="https://www.instagram.com/aquazonewaterpark" target="_blank">Instagram</a>
+      <a href="https://www.facebook.com/aquazonewaterpark" target="_blank">Facebook</a>
+      <a href="https://www.tiktok.com/@aquazonewaterpark" target="_blank">TikTok</a>
     </div>
-  )
+  );
 }
