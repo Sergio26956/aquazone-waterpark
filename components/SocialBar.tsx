d91008422ab04f5fe4df5811271d9075f124d267
sergio <sergiolopezantequera@gmail.com>
2025-07-15 02:47:36 +0200

    Update SocialBar.tsx

--- components/SocialBar.tsx
+++ components/SocialBar.tsx
@@ -1,10 +1,17 @@
 'use client';
+
 export default function SocialBar() {
   return (
-    <div className="flex justify-center gap-4 p-4 bg-gray-800 text-white">
-      <a href="https://www.instagram.com/aquazonewaterpark" target="_blank">Instagram</a>
-      <a href="https://www.facebook.com/aquazonewaterpark" target="_blank">Facebook</a>
-      <a href="https://www.tiktok.com/@aquazonewaterpark" target="_blank">TikTok</a>
+    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-center gap-3">
+      <a href="https://facebook.com/aquazone" target="_blank" rel="noopener noreferrer">
+        <img src="/icons/facebook.svg" alt="Facebook" className="w-10 h-10 hover:scale-110 transition" />
+      </a>
+      <a href="https://instagram.com/aquazone" target="_blank" rel="noopener noreferrer">
+        <img src="/icons/instagram.svg" alt="Instagram" className="w-10 h-10 hover:scale-110 transition" />
+      </a>
+      <a href="https://youtube.com/@aquazone" target="_blank" rel="noopener noreferrer">
+        <img src="/icons/youtube.svg" alt="YouTube" className="w-10 h-10 hover:scale-110 transition" />
+      </a>
     </div>
   );
 }
