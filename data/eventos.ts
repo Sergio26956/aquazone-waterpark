422688d796f9968d2dceefb8dddab3147496b6bc
sergio <sergiolopezantequera@gmail.com>
2025-07-28 04:00:45 +0200

    Create eventos.ts

---
+++ data/eventos.ts
@@ -0,0 +1,16 @@
+export const eventos = [
+  {
+    id: 'ev-01',
+    titulo: 'Evento Playa Marbella',
+    fecha: '2025-08-01',
+    tipo: 'flotante',
+    estado: 'confirmado',
+  },
+  {
+    id: 'ev-02',
+    titulo: 'Ayuntamiento Urban Splash',
+    fecha: '2025-08-10',
+    tipo: 'urbano',
+    estado: 'pendiente',
+  },
+];
