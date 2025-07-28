3173bb20e3863dd86abe09cc10708371e5fceb45
sergio <sergiolopezantequera@gmail.com>
2025-07-28 03:56:04 +0200

    Create galería.tsx

---
+++ pages/galería.tsx
@@ -0,0 +1,9 @@
+import GaleriaCentral from '@/components/GaleriaCentral';
+
+export default function GaleriaPage() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <GaleriaCentral />
+    </main>
+  );
+}
