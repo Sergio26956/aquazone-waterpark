144508cd5eeacc8dbe09f24938cfb60d8c2e3c51
sergio <sergiolopezantequera@gmail.com>
2025-07-28 03:52:20 +0200

    Create galeria.ts

---
+++ data/galeria.ts
@@ -0,0 +1,30 @@
+export const galeriaData = [
+  {
+    id: 'urbano-1',
+    titulo: 'Urbano Splash Circuit',
+    tipo: 'urbano',
+    media: '/media/urbano1.jpg',
+    alt: 'Atracción urbana 1',
+  },
+  {
+    id: 'flotante-1',
+    titulo: 'Flotante Isla Aventura',
+    tipo: 'flotante',
+    media: '/media/flotante1.mp4',
+    alt: 'Aventura flotante',
+  },
+  {
+    id: 'terrestre-1',
+    titulo: 'Tobogán Extremo',
+    tipo: 'terrestre',
+    media: '/media/terrestre1.jpg',
+    alt: 'Tobogán terrestre',
+  },
+  {
+    id: 'kamikaze-1',
+    titulo: 'Kamikaze Jump',
+    tipo: 'kamikaze',
+    media: '/media/kamikaze1.mp4',
+    alt: 'Kamikaze en acción',
+  },
+];
