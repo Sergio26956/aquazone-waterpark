a36b90f2c14d2464dcbbfeedd8342f8067b81a8c
sergio <sergiolopezantequera@gmail.com>
2025-07-28 04:12:50 +0200

    Create page.tsx

---
+++ app/(admin)/dashboard/calendario/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '@/components/admin/DashboardLayout';
+import CalendarioEventos from '@/components/admin/CalendarioEventos';
+
+export default function CalendarioPage() {
+  return (
+    <DashboardLayout>
+      <CalendarioEventos />
+    </DashboardLayout>
+  );
+}
